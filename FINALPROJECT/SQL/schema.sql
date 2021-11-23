8044f84061904c773f3d8e2d8aeb17ea02929003
daniella.tobit <daniella.tobit@gmail.com>
2021-11-23 09:30:02 +0000

    Initial schema

---
+++ FINALPROJECT/SQL/schema.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE productivity_app;
+USE productivity_app;
+
+CREATE TABLE user_info (
+UserID int primary key AUTO_INCREMENT,
+FirstName varchar(50) NOT NULL,
+SecondName varchar(50) NOT NULL,
+PasswordSalt varchar(20) NOT NULL,
+PasswordHash varchar(256) NOT NULL,
+LastLogin datetime
+);
+
+CREATE TABLE game_table (
+GameID int primary key AUTO_INCREMENT,
+GameName varchar(20),
+GameURL varchar(150)
+);
+
+CREATE TABLE sessions (
+SessionID int primary key AUTO_INCREMENT,
+UserID int NOT NULL,
+StartTime datetime,
+EndTime datetime,
+RequestedDuration int NOT NULL,
+FOREIGN KEY (UserID) references user_info(UserID)
+);
+
+CREATE TABLE game_record (
+RecordID int primary key AUTO_INCREMENT,
+UserID int NOT NULL,
+GameID int NOT NULL,
+SessionID int NOT NULL,
+StartTime datetime,
+EndTime datetime,
+FOREIGN KEY (UserID) references user_info(UserID),
+foreign key (GameID) references game_table(GameID),
+foreign key (SessionID) references sessions(SessionID));
