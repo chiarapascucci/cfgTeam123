bcfd05463dd132ab31924b74ff646f50d9bd2a6b
daniellat-git <92787809+daniellat-git@users.noreply.github.com>
2021-11-23 10:06:27 +0000

    Correcting column names

--- FINALPROJECT/SQL/schema.sql
+++ FINALPROJECT/SQL/schema.sql
@@ -4,9 +4,9 @@ USE productivity_app;
 CREATE TABLE user_info (
 UserID int primary key AUTO_INCREMENT,
 FirstName varchar(50) NOT NULL,
-SecondName varchar(50) NOT NULL,
-PasswordSalt varchar(20) NOT NULL,
-PasswordHash varchar(256) NOT NULL,
+LastName varchar(50) NOT NULL,
+PasswordSalt char(20) NOT NULL,
+PasswordHash char(128) NOT NULL,
 LastLogin datetime
 );
 
@@ -20,7 +20,7 @@ CREATE TABLE sessions (
 SessionID int primary key AUTO_INCREMENT,
 UserID int NOT NULL,
 StartTime datetime,
-EndTime datetime,
+EndTime datetime, 
 RequestedDuration int NOT NULL,
 FOREIGN KEY (UserID) references user_info(UserID)
 );
@@ -31,7 +31,8 @@ UserID int NOT NULL,
 GameID int NOT NULL,
 SessionID int NOT NULL,
 StartTime datetime,
-EndTime datetime,
+EndTime datetime, 
 FOREIGN KEY (UserID) references user_info(UserID),
 foreign key (GameID) references game_table(GameID),
 foreign key (SessionID) references sessions(SessionID));
+ 
