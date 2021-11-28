01045d70f359727966a9286ec5d849835b3c3ad4
Chiara <ch.pascucci@gmail.com>
2021-11-28 21:58:22 +0000

    persistent timer implemented

--- FINALPROJECT/routes.py
+++ FINALPROJECT/routes.py
@@ -1,25 +1,16 @@
 from FINALPROJECT import app
-from FINALPROJECT.data_access_functions import create_user_in_db, validate_user, DBConnectionError, _connect_to_db
+from FINALPROJECT.data_access_functions import create_user_in_db, validate_user, DBConnectionError, _connect_to_db, \
+    create_new_session
 from FINALPROJECT.forms import RegistrationForm, LoginForm
-
-
-
 from flask import Flask, jsonify, request, render_template, url_for, redirect, flash
 
 from FINALPROJECT.config import DB_NAME
-
-
-
-
-
 
 
 """
 https://hackersandslackers.com/configure-flask-applications/
 above is link with info about app.config for when you want / encrypt information i think
 """
-
-
 
 
 @app.route('/')
@@ -55,9 +46,17 @@ def login():
     return render_template('login.html', title='login', form=form)
 
 
+@app.route('/tic-tac-toe')
+def tic_tac():
+    return render_template('tic_tac.html', title='Tic Tac Toe!')
+
+@app.route('/tic-tac-toe-ajax')
+def process_tic_tac_toe():
+    pass
+
 @app.route('/start-timer')
 def starttimer():
-    return render_template('select_break_time.html', title='starttimer')
+    return render_template('select_break_time.html', title='Start the Timer')
 
 
 @app.route('/browsegames')
@@ -68,7 +67,7 @@ def browsegames():
 def logsessionstart():
     """will need some way of retrieving user ID"""
     try:
-        connection = _connect_to_db(DB_NAME)
+        connection = _connect_to_db()
         if request.method == 'POST':
 
             print("post request received")
@@ -77,20 +76,15 @@ def logsessionstart():
             user_id = data_dict['user_id']
             start_time = data_dict['start_time']
             req_len = str(data_dict['requested_duration'])
-            query = f"INSERT INTO {DB_NAME}.sessions (UserID, StartTime, RequestedDuration) VALUES ({user_id}, '{start_time}', {req_len});"
-            print(query)
-            my_cur = connection.cursor()
-            my_cur.execute(query)
-            my_cur.close()
-            print("first cursor closed")
-            follow_query = "select * from timer_test.sessions;"
+            session_created = create_new_session(user_id, start_time, req_len)
+            follow_query = f"select * from {DB_NAME}.sessions WHERE UserID = {user_id} ORDER BY SessionID DESC LIMIT 1;"
             print(follow_query)
             other_cur = connection.cursor()
             other_cur.execute(follow_query)
             result = other_cur.fetchall()
-            print("printing result: ", result)
+            print("printing result of query to get latest entry: ", result)
             result = jsonify(result)
-            print("result after jsonify: \n", result)
+
             other_cur.close()
             connection.close()
             print("connection closed")
@@ -103,7 +97,7 @@ def logsessionstart():
 @app.route('/log-session-end', methods=['GET', 'POST'])
 def logsessionend():
     try:
-        connection = _connect_to_db(DB_NAME)
+        connection = _connect_to_db()
         if request.method == 'POST':
             print("post request received")
             print("trying to print json data from request \n", request.get_json())
@@ -113,12 +107,17 @@ def logsessionend():
             user_id = int(data_dict['user_id'])
             end_time = data_dict['end_time']
             session_id = int(data_dict['session_id'])
-            query = f"UPDATE {DB_NAME}.sessions SET EndTime = '{end_time}' WHERE SessionID = {session_id} AND UserID = {user_id}"
+            query = f"UPDATE {DB_NAME}.sessions SET EndTime = '{end_time}' WHERE SessionID = {session_id} AND UserID = {user_id};"
             print(query)
             my_cur = connection.cursor()
             my_cur.execute(query)
             result = my_cur.fetchall()
-
+            connection.commit()
+            check_query = f"SELECT * FROM {DB_NAME}.sessions WHERE SessionID = {session_id};"
+            print("printin check query ", check_query)
+            my_cur.execute(check_query)
+            check_result = my_cur.fetchall()
+            print("printing check result: ", check_result)
             my_cur.close()
             connection.close()
             print("connection closed")
---
+++ FINALPROJECT/static/tic_tac_toe_ajax.js
@@ -0,0 +1,4 @@
+function cell_clicked(cel_num){
+    console.log("cell clicked")
+    console.log(cel_num)
+}
