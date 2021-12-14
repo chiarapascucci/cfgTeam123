942e8a4794c2ead4ba41810baf083c827a1863ab
daniella.tobit <daniella.tobit@gmail.com>
2021-12-14 17:54:59 +0000

    Additional unit test comments

--- FINALPROJECT/test_data_functions.py
+++ FINALPROJECT/test_data_functions.py
@@ -1,31 +1,41 @@
 from unittest import TestCase, main, mock
 import mysql.connector
-from FINALPROJECT.data_access_functions import create_user_in_db, validate_user, get_user_first_last_name, initialise_db, \
+from FINALPROJECT.data_access_functions import create_user_in_db, validate_user, get_user_first_last_name, \
+    initialise_db, \
     update_last_login_time, create_new_session, create_new_game_record, delete_user_from_db, display_total_game_history
 from FINALPROJECT.config import HOST, USER, PASSWORD
 
+"""
+Unit tests using test_db (created in mysql with schema for productivity_app db for testing purposes)
+Tests check:
+a connection is made with the test database
+a new user is created in the test database, returns user_id
+a user can be validated using their username and password, returns user_id
+a user name can be fetched with a user_id
+"""
+
 
 class TestDataFunctions(TestCase):
-
+    # creates tables and inserts user for each test
     def setUp(self):
         self.db = mysql.connector.connect(host=HOST,
-                                     user=USER,
-                                     password=PASSWORD,
-                                     database="test_db")
+                                          user=USER,
+                                          password=PASSWORD,
+                                          database="test_db")
         self.mycursor = self.db.cursor()
         initialise_db(self.db, self.mycursor)
         self.mycursor.execute("DELETE FROM user_info WHERE UserID > 0")
         self.mycursor.execute("""
             INSERT INTO user_info (UserID, UserName, FirstName, LastName, PasswordHash, Email) 
-            VALUES (1, '{}', '{}', '{}', '{}', '{}')""".format("sample_username", "sample_first_name", "sample_last_name",
-                                                            "sample_password", "sample_email"))
+            VALUES (1, '{}', '{}', '{}', '{}', '{}')""".format("sample_username", "sample_first_name",
+                                                               "sample_last_name",
+                                                               "sample_password", "sample_email"))
         self.db.commit()
 
+    # clears tables
     def tearDown(self):
         self.mycursor.execute("DELETE FROM user_info WHERE UserID > 0")
         self.db.commit()
-
-    # Using test_db (created in mysql with schema for productivity_app db for testing purposes)
 
     def test_create_user_in_db_(self):
         test_user_name = "TestUserName"
@@ -40,13 +50,13 @@ class TestDataFunctions(TestCase):
         test_user_id = self.mycursor.fetchone()[0]
         self.assertIsNotNone(test_user_id)
 
+    # validates a sample user (with a user_id of 1)
     def test_validate_user(self):
         user_id = validate_user("sample_username", "sample_password")
         self.assertEqual(user_id, 1)
 
+    # checks name of a sample user (with a user_id of 1)
     def test_get_user_name(self):
         first_name, last_name = get_user_first_last_name(1)
         self.assertEqual("sample_first_name", first_name)
         self.assertEqual("sample_last_name", last_name)
-
-
